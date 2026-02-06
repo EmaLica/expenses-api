439ed4a87582b82d0cc300442036fc645da728f5
EmaLica <emanuelelicatamail@gmail.com>
2026-02-06 18:54:12 +0100

    feat: basic auth starter

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+//Injectable cosi possiamo usare dependency injection
+@Injectable({})
+export class AuthService{
+
+    login(){
+    }
+
+    signup(){
+    }
+
+}
