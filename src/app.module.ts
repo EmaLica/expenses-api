a7090c793fefa99cc43c0393c1c740db2b7b0a40
EmaLica <emanuelelicatamail@gmail.com>
2026-02-06 20:43:20 +0100

    feat: add prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ExpenseModule } from './expense/expense.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ExpenseModule],
+  imports: [AuthModule, UserModule, ExpenseModule, PrismaModule],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 //Injectable cosi possiamo usare dependency injection
 @Injectable({})
 export class AuthService{
+
+    constructor(private prisma : PrismaService){}
 
     signup(){
         return {msg : 'I am signed up'}
